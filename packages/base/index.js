39e39230f79e84395c1b3d3c9fa7b2cc90579284
vq0599 <vq0599@outlook.com>
2020-08-02 13:36:20 +0800

    feat(base): add rules

--- packages/base/index.js
+++ packages/base/index.js
@@ -15,6 +15,16 @@ module.exports = {
     es6: true
   },
   rules: {
+    // 禁止重复的空格
+    "no-multi-spaces": "error",
 
+    // 禁止尾部空格
+    "no-trailing-spaces": "error",
+
+    // import区域下空行
+    "import/newline-after-import": ["error", { "count": 1 }],
+
+    // 缩进2
+    "indent": ["error", 2],
   }
 }
